f394b38e96698c82f85f32b9cfd0bd9bf6eed7d4
MohammedMshal <mohammedmshal2018@gmail.com>
2022-07-28 14:22:19 +0200

    repository name

--- clean_archetecture/lib/wather/domain/entiteis/weather.dart
+++ clean_archetecture/lib/wather/domain/entiteis/weather.dart
@@ -1,8 +1,8 @@
-class Wather {
+class Weather {
   final int id;
   final String cityName;
   final String main;
   final String description;
   final int pressure;
-  Wather(this.id, this.cityName, this.description, this.main, this.pressure);
+  Weather(this.id, this.cityName, this.description, this.main, this.pressure);
 }
