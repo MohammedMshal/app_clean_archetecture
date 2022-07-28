4c5b3e25b3ef704f2865f6c893976109197ab7de
MohammedMshal <mohammedmshal2018@gmail.com>
2022-07-28 15:08:40 +0200

    data model

---
+++ clean_archetecture/lib/wather/data/models/weather_model.dart
@@ -0,0 +1,17 @@
+import 'package:clean_archetecture/wather/domain/entiteis/weather.dart';
+
+class WeatherModel extends Weather {
+  WeatherModel(
+    super.id,
+    super.cityName,
+    super.description,
+    super.main,
+    super.pressure,
+  );
+  factory WeatherModel.fromJson(Map<String, dynamic> json) => WeatherModel(
+      json['id'],
+      json['name'],
+      json['weather'][0]['description'],
+      json['weather'][0]['main'],
+      json['main"']['pressure']);
+}
